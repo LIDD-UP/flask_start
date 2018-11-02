63a47c80b6e39d12dadc0f63d86a52fe55523469
Administrator <1807103336@qq.com>
2018-11-02 17:13:16 +0800

    form test

--- use_template/use_template.py
+++ use_template/use_template.py
@@ -4,9 +4,22 @@
 @file: use_template.py
 @time: 2018/11/2
 """
-from flask import Flask
+from flask import Flask,render_template
 
 # 定义flask实例
 app = Flask(__name__)
 
-#
+@app.route('/test_template/<name>')
+def test_template(name):
+    return render_template('test_template.html',name=name)
+
+
+@app.route('/test_template2')
+def test_template2():
+    dict1 = {'name':'bler'}
+    list1 = ['a','b','c']
+    return render_template('test_template.html',dict1=dict1,list1=list1)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
